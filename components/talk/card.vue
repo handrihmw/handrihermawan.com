5cce45b14b3e917c5a0808cc0f3cf05e6158639b
handrihmw <handri.hermawan@dto.kemkes.go.id>
2023-11-08 23:52:06 +0700

    update card

--- components/talk/card.vue
+++ components/talk/card.vue
@@ -15,7 +15,7 @@ const props = withDefaults(defineProps<TalkData>(), {
   <article
     class="max-w-screen-lg flex flex-col lg:flex-row overflow-hidden bg-slate-100 drop-shadow-md dark:bg-cyan-900/50 rounded-lg hover:bg-slate-100 transform transition dark:hover:bg-cyan-950 dark:hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] hover:drop-shadow-lg lg:mb-0 md:hover:shadow-lg"
   >
-    <div class="basis-1/2 overflow-hidden bg-emerald-600">
+    <div class="basis-1/2 overflow-hidden bg-gray-300">
       <div class="aspect-w-3 aspect-h-2">
         <img
           class="w-full rounded-t-l-md rounded-b-l-md object-cover object-center md:h-36 lg:h-48 transform transition duration-500"
